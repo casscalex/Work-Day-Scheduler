8651130ec138d07721d464d25ce9ee91c73cff49
casscalex <casscalex@outlook.com>
2022-02-01 21:55:44 -0600

    persist functionality broke the script, commented it out

--- Develop/script.js
+++ Develop/script.js
@@ -21,9 +21,26 @@ function taskStatus() {
 taskStatus();
 
 // Save data in local storage
+var saveBtn = $(".saveBtn");
+
 saveBtn.on("click", function() {
     var time = $(this).siblings(".taskTime").text();
     var plan = $(this).siblings(".plan").val();
     localStorage.setItem(time, plan);
 });
 
+// When page is refreshed saved events persist
+// when I use this function it break, not sure why?
+// function saveSchedule() {
+
+//     $(.taskTime).each(function() {
+//         var saveTime = $(this).text();
+//         var savePlan = localStorage.getItem(saveTime);
+
+//         if (saveTime !== null) {
+//             $(this).siblings(".plan").val(savePlan);
+//         }
+//     })
+// };
+
+// saveSchedule();
