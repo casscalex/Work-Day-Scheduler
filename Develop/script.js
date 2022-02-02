8ad338b300906c65dd4929163a0a2e56155bee76
casscalex <casscalex@outlook.com>
2022-02-01 21:45:30 -0600

    added color for status of task, attemted to log in local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,29 @@
 // Display Current Day
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+// Status Color Function
+function taskStatus() {
+    var taskTime = moment().hours();
+
+    $(".time-block").each(function() {
+        var currentTime = parseInt($(this).attr("id"));
+
+        if (currentTime === taskTime) {
+            $(this).addClass("present");
+        } else if (currentTime > taskTime) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+taskStatus();
+
+// Save data in local storage
+saveBtn.on("click", function() {
+    var time = $(this).siblings(".taskTime").text();
+    var plan = $(this).siblings(".plan").val();
+    localStorage.setItem(time, plan);
+});
+
